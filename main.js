b92e686d58fef6e21be278f21eaf80f7ad5b3508
Nagarjuna Kuppala <nagarjunakuppala07@gmail.com>
2018-06-09 18:40:39 -0500

    mongo db connection comment

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ var cors = require('cors');
 const appointmentRoute = require('./routes/appointmentRoute');
 
 var app = express();
+
+//getting mongodb connection
 mongoose.connect('mongodb://localhost:27017/healthmanagement');
 mongoose.connection.on('connected',() =>{
 	console.log("connected to mongodb at port 27017");
