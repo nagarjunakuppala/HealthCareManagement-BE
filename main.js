94a90aa2e71ac32d67c1e0ca4584d7efcbd0a449
Nagarjuna Kuppala <nagarjunakuppala07@gmail.com>
2018-06-09 21:27:28 -0500

    commit

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
-//adding all routes nagarjuna hhhh
 const appointmentRoute = require('./routes/appointmentRoute');
 
 var app = express();
